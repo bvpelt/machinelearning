68fdaf4d8d112f122dafc826b271fa891ed636ab
Bart van Pelt <bart.vanpelt@gmail.com>
2016-05-21 21:09:33 +0200

    First update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # machinelearning
 Example machine learning
+
+[machine learning challanges] (http://tunedit.org/)
+[weka framework](http://www.cs.waikato.ac.nz/~ml/)
---
+++ src/main/java/nl/bsoft/machine/ex01/App.java
@@ -0,0 +1,13 @@
+package nl.bsoft.machine.ex01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
